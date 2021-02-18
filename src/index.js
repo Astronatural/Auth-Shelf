45a82c5a4e6f220808106581e2fae6987607daf5
Quinn Johnson <johnson.fargo87@gmail.com>
2021-02-18 15:55:57 -0600

    added sagas and reducer for GET POST and DELETE

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
-
 import rootReducer from './redux/reducers/_root.reducer'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas/_root.saga'; // imports ./redux/sagas/index.js
-
+import { takeEvery, put } from 'redux-saga/effects';
 import App from './components/App/App';
+
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -18,6 +18,9 @@ const sagaMiddleware = createSagaMiddleware();
 const middlewareList = process.env.NODE_ENV === 'development' ?
   [sagaMiddleware, logger] :
   [sagaMiddleware];
+
+
+// reducer used to send out the selected items
 
 const store = createStore(
   // tells the saga middleware to use the rootReducer
